8c94375605b49b02bb910c1fb9a3631b3885c905
agudelojuanesteban69-sudo <agudelojuanesteban69@gmail.com>
2026-06-02 16:40:56 -0500

    get_user_information : Modify form

---
+++ js/pedidos.js
@@ -0,0 +1,6 @@
+let Nombre = document.getElementById("Nombre").value;
+let Dirección = document.getElementById("Dirección").value;
+let Teléfono = document.getElementById("Teléfono").value;
+let Email = document.getElementById("Email").value;
+let metodo_pago = document.getElementById("metodo_pago").value;
+let pedidos = document.getElementById("pedidos").value;
---
+++ js/usuarios.js
@@ -0,0 +1,9 @@
+let Nombre = document.getElementById("Nombre").value;
+let Email = document.getElementById("Email").value;
+let Número_celular = document.getElementById("Número_celular").value;
+let pedidos = document.getElementById("pedidos").value;
+let fecha_nacimiento = document.getElementById("fecha_nacimiento").value;
+let Dirección = document.getElementById("Dirección").value;
+let genero = document.getElementById("genero ").value;
+let acepto = document.getElementById("acepto").value;
+let laptop = document.querySelector('input[name="laptop"]:checked').value;
